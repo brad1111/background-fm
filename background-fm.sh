ad5ac8274df19503850ef6306d6a4dea29466c68
Bradley Eaton <mail.thebradad1111@gmail.com>
2020-09-28 00:35:20 +0100

    Now preserves order of albums

--- background-fm.sh
+++ background-fm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#Setup directories and stuff
 NAME=background-fm
 if [ -z $HOME ] && ([ -z $XDG_DATA_HOME ] || [ -z $XDG_CACHE_HOME ]); then 
 	echo You need to have either \$HOME environment variable set or both \$XDG_DATA_HOME and \$XDG_CACHE_HOME to use this program
@@ -26,15 +28,21 @@ if [ ! -e $APIKEYLOCATION ]; then
 fi
 APIKEY=$(cat $APIKEYLOCATION)
 
+#Get number of albums to get
+##First get screen resolution
+
+
 ALBUM_COUNT=20
 USERNAME=thebradad1111
 URL="http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${USERNAME}&api_key=${APIKEY}&period=1month&limit=${ALBUM_COUNT}"
-echo URL:$URL
+#Get image urls from XML
 IMAGE_URLS=$(curl $URL | grep '<image size="extralarge"' | sed -r 's/<\/?\w(\w|| )+(="\w+")?>//g' | sed 's/ //g')
-declare -a FILE_NAMES
+#Store file locations so we can keep order of albums
+declare -a FILE_LOCATIONS
 for IMAGE_URL in $IMAGE_URLS; do
 	FILENAME=$(grep -Po '\w+\.\w+$' <<< $IMAGE_URL)
 	FILE_LOCATION=$CACHE_DIR/$FILENAME
+	FILE_LOCATIONS+=("${FILE_LOCATION}")
 	if [ ! -e $FILE_LOCATION ]; then
 		curl $IMAGE_URL -o $FILE_LOCATION 
 	fi
@@ -44,4 +52,4 @@ OUTIMG=$CACHE_DIR/out.png
 if [ -e $OUTIMG ]; then
 	rm $OUTIMG
 fi
-montage -geometry +0+0 $CACHE_DIR/* $OUTIMG
+montage -geometry +0+0 ${FILE_LOCATIONS[*]} $OUTIMG
